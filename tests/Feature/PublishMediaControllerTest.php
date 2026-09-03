b80c48ac5dc1f789df24b96f6ae81bbc2053eff4
Harun R. Rayhan <harun.b13@gmail.com>
2026-09-03 12:59:13 +0600

    Trust forwarded scheme for signed media URLs

--- tests/Feature/PublishMediaControllerTest.php
+++ tests/Feature/PublishMediaControllerTest.php
@@ -42,6 +42,43 @@ class PublishMediaControllerTest extends TestCase
             ->assertHeader('Content-Type', 'image/png');
     }
 
+    public function test_signed_url_validates_https_scheme_from_reverse_proxy(): void
+    {
+        Storage::fake('scratchpad');
+
+        $workspace = Workspace::factory()->create();
+        $post = Post::factory()->for($workspace)->create();
+        $media = MediaAsset::factory()->for($workspace)->create([
+            'disk' => 'scratchpad',
+            'path' => 'posts/proxied.png',
+            'mime' => 'image/png',
+            'original_filename' => 'proxied.png',
+        ]);
+        Storage::disk('scratchpad')->put('posts/proxied.png', 'png-bytes');
+
+        Attachment::factory()->for($post, 'attachable')->for($media)->create();
+
+        URL::forceRootUrl('https://cm.harun.dev');
+        URL::forceScheme('https');
+        $url = URL::temporarySignedRoute(
+            'publish-media.post',
+            now()->addHour(),
+            ['post' => $post->id, 'mediaAsset' => $media->id],
+        );
+
+        $response = $this->withServerVariables([
+            'HTTPS' => 'off',
+            'SERVER_PORT' => 80,
+        ])->withHeaders([
+            'Host' => 'cm.harun.dev',
+            'X-Forwarded-Proto' => 'https',
+            'X-Forwarded-Host' => 'cm.harun.dev',
+        ])->get($url);
+
+        $response->assertOk()
+            ->assertHeader('Content-Type', 'image/png');
+    }
+
     public function test_unsigned_url_is_rejected(): void
     {
         $workspace = Workspace::factory()->create();
